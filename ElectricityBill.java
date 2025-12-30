0db4ac2c4e0c0e60696d7d37504388a95badd1ee
immanuvelponrani <immanuvel7598@gmail.com>
2025-12-30 13:29:35 +0530

    condition program

---
+++ ElectricityBill.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int units = sc.nextInt();
+        int bill;
+
+        if (units <= 100) {
+            bill = units * 5;
+        } else if (units <= 200) {
+            bill = units * 7;
+        } else {
+            bill = units * 10;
+        }
+
+        System.out.println("Electricity Bill: ₹" + bill);
+    }
+}
